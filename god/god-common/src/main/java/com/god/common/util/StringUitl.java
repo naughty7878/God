9855350ac3cb7891573a4b42bf48e2ee2926aac1
god <naughty7878@163.com>
2018-09-08 00:06:55 +0800

    构建maven多模块项目

--- god/god-common/src/main/java/com/god/common/util/StringUitl.java
+++ god/god-common/src/main/java/com/god/common/util/StringUitl.java
@@ -8,4 +8,27 @@ package com.god.common.util;
  */
 public class StringUitl {
 
+	/**
+	 * 判断字符串是否是空白的
+	 * 以下情况为真
+	 * 	1、null
+	 *  2、”“
+	 *  3、”  “
+	 * @param str
+	 * @return
+	 */
+	public static boolean isBlank(String str) {
+		return str == null || "".equals(str.trim().length() == 0);
+	}
+	
+	/**
+	 * 判断字符串是否为空
+	 * null || “”
+	 * @param str
+	 * @return
+	 */
+	public static boolean isEmpty(String str) {
+		return str == null || "".equals(str);
+	}
+	
 }
